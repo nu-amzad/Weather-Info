722f485c3b1a01f5e5881cd33d747fa97213e532
Md Nurul Amzad <nurulamzad788@gmail.com>
2025-04-15 13:11:48 +0600

    live-location added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let searchbar = document.getElementById('search-box');
 let btn = document.getElementById('search-btn');
 let temp = document.getElementById('temp');
 let weat = document.getElementById('weather');
+let livloc = document.getElementById('live-loc');
 let cityName;
 
 async function getdata(value){
@@ -14,6 +15,28 @@ async function getdata(value){
   }
 }
 
+async function getdataLL(value1, value2){
+  try{
+  let datas = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${value1}&lon=${value2}&appid=4a0470befc3535138ebf692dd9865e7c&lang=en`);
+  return await datas.json();
+  }
+  catch(error){
+    return console.log(error);
+  }
+}
+
+
+async function locationSuccess(position){
+  const llreturnedData= await getdataLL(position.coords.latitude, position.coords.longitude);
+  let tocel = llreturnedData.main.temp - 273.15;
+  temp.textContent = `Tempreature: ${tocel.toFixed(2)}°C`;
+  weat.textContent = `Weather: ${llreturnedData.weather[0].description.toUpperCase()}`;
+}
+
+function locationFailed(){
+  console.log ('Cannto Access Location');
+}
+
 btn.addEventListener('click', async()=>{
   cityName = searchbar.value;
   searchbar.value = '';
@@ -22,4 +45,8 @@ btn.addEventListener('click', async()=>{
   temp.textContent = `Temprature: ${tocel.toFixed(2)}°C`;
   weat.textContent = `Weather: ${returnedData.weather[0].description.toUpperCase()}`;
 
+})
+
+livloc.addEventListener('click', async ()=>{
+  navigator.geolocation.getCurrentPosition(locationSuccess,locationFailed);
 })
